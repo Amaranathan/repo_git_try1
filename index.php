6ebdc157bb6f541f4cc35fe15855529cf9d53d9f
Amaranathan <aradhakrishnan@fleetstudio.com>
2018-12-27 11:58:08 +0530

    Amar's first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Vinayaga<br>';
+        echo 'This is to try Github<br>';
+        echo 'Project name : git_try1<br>';
+        ?>
+    </body>
+</html>
