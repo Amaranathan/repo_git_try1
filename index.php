6a696e16744befd06e14c0da2ca85e5cd4402ef0
Amaranathan <aradhakrishnan@fleetstudio.com>
2018-12-27 15:09:24 +0530

    Added a echo statement for trial

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ and open the template in the editor.
         echo 'Vinayaga<br>';
         echo 'This is to try Github<br>';
         echo 'Project name : git_try1<br>';
+		echo 'New line introduced by new_branch1<br>';
         ?>
     </body>
 </html>
